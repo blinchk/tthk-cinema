54129b591332ad16c459a60c5cae8803660022ce
opilane <opilane@HP-CZC6297BB7>
2021-02-03 15:06:14 +0200

    Add GetMovie, GetPlaces, GetSessions methods to DataHandler.

--- CinemaForm.cs
+++ CinemaForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using tthk_kinoteater.Models;
 
 namespace tthk_kinoteater
 {
--- DataHandler.cs
+++ DataHandler.cs
@@ -1,5 +1,8 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
+using tthk_kinoteater.Models;
 
 namespace tthk_kinoteater
 {
@@ -7,6 +10,7 @@ namespace tthk_kinoteater
     {
         private readonly SqlConnection connection = new SqlConnection(
             @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename =|DataDirectory|\AppData\cinema.mdf; Integrated Security = True");
+        private SqlCommand command;
         
         public DataHandler()
         {
@@ -17,6 +21,77 @@ namespace tthk_kinoteater
         {
             if (connection != null && connection.State == ConnectionState.Open)
                 connection.Close();
+        }
+
+        public Movie GetMovie(int id)
+        {
+            command = new SqlCommand("SELECT * FROM Movies WHERE Id = @id");
+            command.Parameters.AddWithValue("@id", id);
+            Movie movie = new Movie();
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    movie.Id = id;
+                    movie.Title = reader["Title"].ToString();
+                    movie.Year = Convert.ToInt32(reader["Email"].ToString());
+                    movie.Director = reader["Director"].ToString();
+                    movie.Duration = TimeSpan.FromMinutes(Convert.ToInt32(reader["Duration"].ToString()));
+                }
+            }
+            return movie;
+        }
+
+        public Hall GetHall(int id)
+        {
+            command = new SqlCommand("SELECT * FROM Halls");
+            Hall hall = new Hall();
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    hall.Id = id;
+                    hall.Title = reader["Title"].ToString();
+                }
+            }
+            return hall;
+        }
+
+        public List<Place> GetPlaces(Hall hall)
+        {
+            List<Place> places = new List<Place>();
+            command = new SqlCommand("SELECT * FROM Places WHERE Hall = @id");
+            command.Parameters.AddWithValue("@id", hall.Id);
+            Place place = new Place();
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    place.Id = Convert.ToInt32(reader["Id"].ToString());
+                    place.Number = Convert.ToInt32(reader["Number"].ToString());
+                    place.Row = Convert.ToInt32(reader["Row"].ToString());
+                    places.Add(place);
+                }
+            }
+            return places;
+        }
+
+        public List<Session> GetSessions()
+        {
+            List<Session> sessions = new List<Session>();
+            command = new SqlCommand("SELECT * FROM Sessions;");
+            Session session = new Session();
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    session.Id = Convert.ToInt32(reader["Id"].ToString());
+                    session.Movie = GetMovie(Convert.ToInt32(reader["Id"].ToString()));
+                    session.Time = Convert.ToDateTime(reader["Time"].ToString());
+                    sessions.Add(session);
+                }
+            }
+            return sessions;
         }
     }
 }
