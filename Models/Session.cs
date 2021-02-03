e46af9594d2e028cb230bf5413fb9e2bdc89d3bd
Opilane TTHK <opilane@tthk.ee>
2021-02-03 13:35:17 +0200

    Add project files.

---
+++ Models/Session.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tthk_kinoteater.Models
+{
+    class Session
+    {
+    }
+}
