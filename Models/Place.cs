7f300d9ddd8fa44ef4f8de847bfc3aaf22a00b9d
Nikolas Laus <bchik3@gmail.com>
2021-02-16 15:53:22 +0200

    Delete Id field from Place model.

--- Models/Place.cs
+++ Models/Place.cs
@@ -6,7 +6,6 @@ namespace tthk_kinoteater.Models
 {
     class Place
     {
-        public int Id { get; set; }
         public int Number { get; set; }
         public int Row { get; set; }
         public Hall Hall { get; set; }
