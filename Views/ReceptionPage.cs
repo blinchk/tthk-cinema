1b21d43e8af967b90a87cb957a597568ddbaf529
Nikolas Laus <bchik3@gmail.com>
2021-03-03 13:08:27 +0200

    Refactor code.

---
+++ Views/ReceptionPage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using tthk_kinoteater.Models;
+
+namespace tthk_kinoteater.Views
+{
+    public partial class ReceptionPage : UserControl
+    {
+        private List<Place> busyPlaces;
+        
+        public ReceptionPage(List<Place> busyPlaces)
+        {
+            this.busyPlaces = busyPlaces;
+            InitializeComponent();
+        }
+    }
+}
